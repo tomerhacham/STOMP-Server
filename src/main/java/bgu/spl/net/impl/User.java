1ac02ba0b7eba1df982fe6744dbd88a263e50e65
Sagi Dahan <sagidaha@post.bgu.ac.il>
2020-01-07 20:35:28 +0200

    add logic

--- src/main/java/bgu/spl/net/impl/User.java
+++ src/main/java/bgu/spl/net/impl/User.java
@@ -4,11 +4,15 @@ public class User {
 
     //Fields:
     private final Integer id;
+    private String Password;
+    private String UserName;
     private boolean loggin; //indicate if the user is log in
 
-    public User(Integer id) {
+    public User(Integer id, String password, String userName) {
         this.id = id;
         loggin=false;
+        this.Password=password;
+        this.UserName=userName;
     }
 
     public Integer getId() {
@@ -16,5 +20,9 @@ public class User {
     }
     public void login(){loggin=true;}
     public void logout(){loggin=false;}
-
+    public boolean isLoggin(){return this.loggin;}
+    public String getPassword(){ return this.Password; }
+    public String getUserName() {
+        return this.UserName;
+    }
 }
